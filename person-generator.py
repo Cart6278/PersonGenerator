5e75486ec9209a585d08bb3cd2ad5dd58368a775
Kirsten C <cartekir@oregonstate.edu>
2021-02-06 21:01:27 -0800

    Initial commit

---
+++ person-generator.py
@@ -0,0 +1,8 @@
+# import Tkinter
+from tkinter import *
+
+# loading Tkinter adapted from: https://stackoverflow.com/questions/53797598/how-to-install-tkinter-with-pycharm
+
+top = Tk()
+# widget code
+top.mainloop()

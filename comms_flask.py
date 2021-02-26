33caa6c890ccb3f045ab7854f68fd8a32df4eb0b
Kirsten <49821520+Cart6278@users.noreply.github.com>
2021-02-26 13:25:30 -0800

    Updated webapp and changed person generator name

--- comms_flask.py
+++ comms_flask.py
@@ -2,24 +2,30 @@ import os
 from flask import Flask
 from flask import request
 from flask import jsonify
-from person-generator import main # this import is not working, why?
+from persongenerator import main  # this import is not working, why?
 
 webapp = Flask(__name__)
 
-@webapp.route('/get', methods=['GET'])
+
+@webapp.route('/', methods=['GET', 'POST'])
 def listener():
-    state_find = request.args.get("state")
-    num_addr = request.args.get("nums")
-    return_csv = 0
-    if not state_find or not num_addr:
-        return "Error: Missing one or both required inputs"
+    if request.method == 'GET':
+        state_find = request.args.get("state")
+        num_addr = request.args.get("nums")
+        if not state_find or not num_addr:
+            return "Error: Missing one or both required inputs"
+        return_csv = 0
+        adds_generated = main(state_find, num_addr, return_csv)
+        return jsonify(adds_generated)
+    if request.method == 'POST':
+        state_find = request.args.get("state")
+        num_addr = request.args.get("nums")
+        if not state_find or not num_addr:
+            return "Error: Missing one or both required inputs"
+        return_csv = 0
+        adds_generated = main(state_find, num_addr, return_csv)
+        return jsonify(adds_generated)
 
-    adds_generated = main(state_find, num_addr, return_csv)
-    return jsonify(adds_generated)
-
-@webapp.route('/')
-def index():
-    return "This is the index"
 
 if __name__ == '__main__':
     webapp.run(port=6001)
